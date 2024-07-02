1261464db3ee4e55c2f91d01eeb3cf5ba3fc3a3c
infern <semenmochalov55@gmail.com>
2024-07-02 16:09:33 +0700

    Init

---
+++ app/Models/User.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'profile_picture',
+        'bio',
+        'location',
+        'experience',
+        'github_link',
+        'linkedin_link'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    public function skills()
+    {
+        return $this->belongsToMany(Skill::class, 'user_skills');
+    }
+
+    public function seekingSkills()
+    {
+        return $this->belongsToMany(Skill::class, 'user_seeking_skills');
+    }
+
+    public function messagesSent()
+    {
+        return $this->hasMany(Message::class, 'sender_id');
+    }
+
+    public function messagesReceived()
+    {
+        return $this->hasMany(Message::class, 'receiver_id');
+    }
+
+    public function userMatches()
+    {
+        return $this->hasMany(UserMatch::class);
+    }
+
+    public function matchedUsers()
+    {
+        return $this->belongsToMany(User::class, 'user_matches', 'user_id', 'matched_user_id')
+            ->withPivot('status')
+            ->withTimestamps();
+    }
+}
